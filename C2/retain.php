01e893be00906f3cb708b17fb98d3437712583ca
Tansif Tushan <tansiftushan@gmail.com>
2026-04-12 18:42:53 +0600

    Lab Final_1

---
+++ C2/retain.php
@@ -0,0 +1,26 @@
+<?php
+    if(isset($_POST['submit'])){
+        $email = $_POST['email'];
+
+        if($email == ""){
+            echo "null email!";
+        } else {
+            echo "Your email is: " . $email;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Email</title>
+</head>
+<body>
+    <form method="post" action="">
+        EMAIL: <input type="text" name="email" 
+                      value="<?php if(isset($_POST['email'])){echo $_POST['email'];} ?>"/> 
+        <br><br>
+        <input type="submit" name="submit" value="Submit"/>
+    </form>
+</body>
+</html>
